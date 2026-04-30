4404c4eb2ced08ef29b83aa42c9af60f4d5e9595
mishkatjamal <mishkatjamal13@gmail.com>
2026-04-30 20:49:26 +0530

    promises

--- app.js
+++ app.js
@@ -1,37 +1,22 @@
-// *Oops topic
-// ^ classes extend super
+// *promises
 
-class User{
-    constructor(name,address,email,color){
-        this.name = name
-        this.address = address
-        this.email = email
-        this.color = color
-        this.role = "user"
-    }
+let pr = new Promise((res,rej)=>{
 
-    write(text){
-        let h1 = document.createElement("h1")
-        h1.textContent = `${this.role}: ${text}`
-        h1.style.color = this.color
+    setTimeout(() => {
+        let rn = Math.floor(Math.random()*10)
+        if (rn>5) {
+            return res("resolved "+ rn)
+        } else {
+            return rej("reject "+ rn)
+        }
+    }, 3000);
+})
 
-        document.body.appendChild(h1)
-    }
-}
+pr.then((val)=>{
+    console.log(val);
+    
+})
+.catch((val)=>{
+console.log(val);
 
-
-class Admin extends User{
-    constructor(name,address,email,color){
-        super(name,address,email,color)
-        this.role = "Admin"
-    }
-
-    remove(){
-        document.querySelectorAll("h1").forEach((elem)=>{
-            elem.remove()
-        })
-    }
-}
-let u1 = new User("mishkat","kamptee","email1@.com","black")
-let u2 = new User("jamal","nagpur","emaisfdl1@.com","red")
-let a1 = new Admin("keepr","nagpur","emaisfdl1@.com","green")
+})
