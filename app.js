9a36745cb203ab8b46b0e9ee694ea0ba501dd631
mishkatjamal <mishkatjamal13@gmail.com>
2026-04-30 17:44:17 +0530

    class with extends and super

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 // *Oops topic
-// ^ classes 
+// ^ classes extend super
 
-class CreatePencil{
-    constructor(name,company,price,color){
+class User{
+    constructor(name,address,email,color){
         this.name = name
-        this.company= company
-        this.price = price
+        this.address = address
+        this.email = email
         this.color = color
+        this.role = "user"
     }
 
     write(text){
         let h1 = document.createElement("h1")
-        h1.textContent = text
+        h1.textContent = `${this.role}: ${text}`
         h1.style.color = this.color
+
         document.body.appendChild(h1)
     }
 }
 
 
-let p1 = new CreatePencil("natraj","natraj",10,"red")
-let p2 = new CreatePencil("natraj","natraj",10,"black")
+class Admin extends User{
+    constructor(name,address,email,color){
+        super(name,address,email,color)
+        this.role = "Admin"
+    }
+
+    remove(){
+        document.querySelectorAll("h1").forEach((elem)=>{
+            elem.remove()
+        })
+    }
+}
+let u1 = new User("mishkat","kamptee","email1@.com","black")
+let u2 = new User("jamal","nagpur","emaisfdl1@.com","red")
+let a1 = new Admin("keepr","nagpur","emaisfdl1@.com","green")
