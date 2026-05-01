47d0461851f7c8a0c3c19985f223f5acd459b98f
mishkatjamal <mishkatjamal13@gmail.com>
2026-05-02 00:51:03 +0530

    iffe

--- app.js
+++ app.js
@@ -1,48 +1,35 @@
+// *iffe
 
-fetch("https://randomuser.me/api/?results=5")
-.then((raw) => raw.json())
-.then((data)=>{
-    // console.log(data.results);
-    
-    let main =document.querySelector("#main")
-    data.results.forEach((elem) => {
+let Bank = (function (){
+    let balance = 1000
+
+    function checkBalance(){
+        console.log(`yor current balance is ${balance}`);
         
-        // Card container
-const card = document.createElement("div");
-card.className = "w-72 bg-white rounded-2xl shadow-lg p-5 text-center";
+    }
 
-// Image
-const img = document.createElement("img");
-img.src = elem.picture.large;
-img.alt = "User Photo";
-img.className = "w-24 h-24 rounded-full mx-auto object-cover border-4 border-blue-500";
-
-// Name
-const name = document.createElement("h2");
-name.className = "mt-4 text-xl font-bold text-gray-800";
-name.textContent = elem.name.first +" "+ elem.name.last;
-
-// Email
-const email = document.createElement("p");
-email.className = "mt-1 text-sm text-gray-500";
-email.textContent = elem.email;
-
-// Gender
-const gender = document.createElement("div");
-gender.className = "mt-4 inline-block px-4 py-1 text-sm font-medium bg-blue-100 text-blue-600 rounded-full";
-gender.textContent = elem.gender;
-
-// Append sab kuch card me
-card.appendChild(img);
-card.appendChild(name);
-card.appendChild(email);
-card.appendChild(gender);
-
-// Root me card add karo
-main.appendChild(card);
+    function setBalance(val){
+        balance+= val
+        console.log(`yor current balance is ${balance}`);
         
-    });
-})
-.catch((err)=>{
+    }
 
-})
+    function withdrawl(val){
+        if (val <= balance) {
+            balance-= val
+            console.log(`yor current balance is ${balance}`);
+            
+        }
+    }
+
+    return{
+        checkBalance,
+        setBalance,
+        withdrawl
+    }
+   
+})();
+
+Bank.checkBalance();
+Bank.setBalance(200)
+Bank.withdrawl(100)
