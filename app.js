c1e257fd9828962887025dc7627fcf34a7f8743c
mishkatjamal <mishkatjamal13@gmail.com>
2026-05-02 01:41:39 +0530

    observer pattern

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
-// *factory function pattern
+// * observer pattern
 
-function factory(name,price){
+class YoutubeChannel{
+    constructor(){
+        this.subscribers = []
+    }
 
-    let stock = 10
-    return{
-        name,
-        price,
-        checkStock(){
-            console.log(`${stock} is avalaible`);
-            
-        },
-        buy(qty){
-            if (qty<=stock) {
-                stock -= qty
-                console.log(`${qty} you booked now we have ${stock} left`);
-                
-            } else {
-                console.error(`we have only ${stock} stock right now`);
-                
-            }
-        },
-        refill(qyt){
-            stock+= qyt
-            console.log(`now we have total ${stock} stock`);
-            
-        }
+    subscribe(user){
+        this.subscribers.push(user)
+        user.update(`${user.name} you subscribe the channel`)
+    }
+    unsubscribe(user){
+        this.subscribers = this.subscribers.filter((sub)=>{
+            return sub !== user
+        })
+        user.update(`${user.name} you unsubscribe the channel`)
+    }
+    notify(message){
+        this.subscribers.forEach((sub)=>{
+            sub.update(message)
+        })
+    }
+}
+
+class User{
+    constructor(name){
+        this.name = name
+    }
+    update(data){
+        console.log(`${this.name} ${data}`);
+        
     }
 }
 
 
-let iphone = factory("iphone17",40000)
-let fenta = factory("fenta",20)
+
+let gk = new YoutubeChannel()
+let user1 = new User("mishkat")
+let user2 = new User("jamal")
+
+gk.subscribe(user1)
+gk.subscribe(user2)
+
+
+gk.notify("new video dala hu main")
+gk.unsubscribe(user2)
+
+gk.notify("post hai ek dekho")
+
