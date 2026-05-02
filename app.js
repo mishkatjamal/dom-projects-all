35fa4460accabddbd3742bc67fdb380934f1b266
mishkatjamal <mishkatjamal13@gmail.com>
2026-05-02 12:33:16 +0530

    intersection observer

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-// * throttle
+// * intersection observer
 
-let inp = document.querySelector("input")
-
-function throttle(fnc,delay){
-    let timer = 0
-return function(...arg){
-    let now = Date.now()
-    if (now - timer >= delay) {
-        timer = now
-        fnc(...arg)
+let imgs = document.querySelectorAll("img")
+const observer = new IntersectionObserver(
+    function(entires,observer){
+        entires.forEach((entry)=>{
+            if (entry.isIntersecting) {
+                const img = entry.target
+                img.src = img.dataset.src
+                img.classList.add("loaded")
+                observer.unobserve(entry)
+            }
+        })
+    },{
+        root:null,
+        threshold:0.1
     }
-}
-}
+)
 
-inp.addEventListener("input",throttle(function(){
-    console.log("hello");
-    
-},1000))
+
+imgs.forEach((img)=>{
+    observer.observe(img)
+})
