01ae0ccb04c6a820ce638d4c394b5ebfcd52d16c
mishkatjamal <mishkatjamal13@gmail.com>
2026-05-02 11:56:09 +0530

    debouncing

--- app.js
+++ app.js
@@ -1,49 +1,18 @@
-// * observer pattern
+// * debouncing
 
-class YoutubeChannel{
-    constructor(){
-        this.subscribers = []
-    }
+let inp = document.querySelector("input")
 
-    subscribe(user){
-        this.subscribers.push(user)
-        user.update(`${user.name} you subscribe the channel`)
-    }
-    unsubscribe(user){
-        this.subscribers = this.subscribers.filter((sub)=>{
-            return sub !== user
-        })
-        user.update(`${user.name} you unsubscribe the channel`)
-    }
-    notify(message){
-        this.subscribers.forEach((sub)=>{
-            sub.update(message)
-        })
+function debouncing(fnc,delay){
+    let timer;
+    return function(...arg){
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fnc(...arg)
+        }, delay);
     }
 }
 
-class User{
-    constructor(name){
-        this.name = name
-    }
-    update(data){
-        console.log(`${this.name} ${data}`);
-        
-    }
-}
-
-
-
-let gk = new YoutubeChannel()
-let user1 = new User("mishkat")
-let user2 = new User("jamal")
-
-gk.subscribe(user1)
-gk.subscribe(user2)
-
-
-gk.notify("new video dala hu main")
-gk.unsubscribe(user2)
-
-gk.notify("post hai ek dekho")
-
+inp.addEventListener("input",debouncing(function(){
+    console.log("hello");
+    
+},1000))
